9b52a33cf5e85d4e69e41618a89a15170087df79
Vijith VS <vijithvenganoor@gmail.com>
2015-12-30 19:25:37 +0530

    Project

---
+++ Querry Helper/querry helper/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Querry_Helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtScema = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTable = new System.Windows.Forms.TextBox();
+            this.btnGO = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtSP = new System.Windows.Forms.RichTextBox();
+            this.txtModel = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtParameter = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Table Name";
+            // 
+            // txtScema
+            // 
+            this.txtScema.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtScema.Location = new System.Drawing.Point(140, 45);
+            this.txtScema.Name = "txtScema";
+            this.txtScema.Size = new System.Drawing.Size(228, 20);
+            this.txtScema.TabIndex = 3;
+            this.txtScema.Text = "atozdental";
+            this.txtScema.Leave += new System.EventHandler(this.txtScema_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Scema Name";
+            // 
+            // txtTable
+            // 
+            this.txtTable.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtTable.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.txtTable.Location = new System.Drawing.Point(140, 74);
+            this.txtTable.Name = "txtTable";
+            this.txtTable.Size = new System.Drawing.Size(228, 20);
+            this.txtTable.TabIndex = 5;
+            // 
+            // btnGO
+            // 
+            this.btnGO.Location = new System.Drawing.Point(140, 108);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(75, 23);
+            this.btnGO.TabIndex = 6;
+            this.btnGO.Text = "GO";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(236, 108);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtSP
+            // 
+            this.txtSP.Location = new System.Drawing.Point(39, 161);
+            this.txtSP.Name = "txtSP";
+            this.txtSP.Size = new System.Drawing.Size(807, 196);
+            this.txtSP.TabIndex = 8;
+            this.txtSP.Text = "";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(39, 388);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(333, 221);
+            this.txtModel.TabIndex = 9;
+            this.txtModel.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Stored Procedure";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 372);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Data Model";
+            // 
+            // txtParameter
+            // 
+            this.txtParameter.Location = new System.Drawing.Point(404, 388);
+            this.txtParameter.Name = "txtParameter";
+            this.txtParameter.Size = new System.Drawing.Size(442, 221);
+            this.txtParameter.TabIndex = 12;
+            this.txtParameter.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(401, 372);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Parameter";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(96, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Server";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(140, 11);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 20);
+            this.txtServer.TabIndex = 15;
+            this.txtServer.Text = "172.16.0.29";
+            this.txtServer.Leave += new System.EventHandler(this.txtScema_Leave);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(246, 14);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(272, 11);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 17;
+            this.txtID.Text = "root";
+            this.txtID.Leave += new System.EventHandler(this.txtScema_Leave);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(449, 11);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(100, 20);
+            this.txtPwd.TabIndex = 19;
+            this.txtPwd.Text = "root";
+            this.txtPwd.Leave += new System.EventHandler(this.txtScema_Leave);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(387, 14);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(938, 626);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtParameter);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtSP);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGO);
+            this.Controls.Add(this.txtTable);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtScema);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Querry Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtScema;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTable;
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox txtSP;
+        private System.Windows.Forms.RichTextBox txtModel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox txtParameter;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Querry Helper/querry helper/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Querry_Helper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public MySqlConnection connection()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            try
+            {
+                string cons = "server="+txtServer.Text+";User Id="+txtID.Text+";password="+txtPwd.Text+";Persist Security Info=True;database="+txtScema.Text+"";
+                String connectionString = cons;
+                conn.ConnectionString = connectionString;
+                return conn;
+            }
+            catch (Exception ex)
+            {                
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+        public DataTable getTableQry(string Querry)
+        {
+            DataTable dt = new DataTable();
+            MySqlConnection con = connection();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(Querry, con);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+            }
+            return dt;
+        }
+
+        public bool checkConnection()
+        {
+            bool check = true;
+            MySqlConnection con = connection();
+            try
+            {
+                con.Open();
+            }
+            catch (MySqlException ex)
+            {
+
+                switch (ex.Number)
+                {
+                    case 4060: // Invalid Database 
+                        System.Windows.Forms.MessageBox.Show("Invalid Database");
+                        break;
+                    case 18456: // Login Failed 
+                        System.Windows.Forms.MessageBox.Show("Login Failed ");
+                        break;
+                    default:
+                        //....
+                        break;
+                }
+            }
+            return check;
+        }
+
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtServer.Text == "" && txtID.Text == "" && txtPwd.Text == "")
+                    MessageBox.Show("Enter a server, ID and password");
+                else
+                {
+                    if (txtScema.Text == "")
+                        MessageBox.Show("Enter a schema name");
+                    else
+                        if (txtTable.Text == "")
+                            MessageBox.Show("Enter the Table Name");
+                        else
+                            go();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void go()
+        {
+            string qry = @"SELECT   TABLE_NAME
+               , COLUMN_NAME
+               , DATA_TYPE
+               , CHARACTER_MAXIMUM_LENGTH
+               , CHARACTER_OCTET_LENGTH 
+               , NUMERIC_PRECISION 
+               , NUMERIC_SCALE AS SCALE
+               , COLUMN_DEFAULT
+               , IS_NULLABLE
+               ,TABLE_SCHEMA
+               FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + txtTable.Text.Trim().ToString() + "' ";
+            if (txtScema.Text.Trim().ToString() != "")
+                qry += " and TABLE_SCHEMA='" + txtScema.Text + "'";
+            DataTable dt = getTableQry(qry);
+            if (dt.Rows.Count > 0)
+            {
+                storedPros(dt);
+                dataModel(dt);
+                parameter(dt);
+            }
+            else
+                MessageBox.Show("No Data Available");
+        }
+
+        private void parameter(System.Data.DataTable dt)
+        {
+            string pm = "";
+            char ch = '"';
+            pm += @"string qry = @"+ch.ToString()+"addEditSystemSetting"+ch.ToString()+@";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = qry;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;";
+            
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (dt.Rows[i]["DATA_TYPE"].ToString() == "int")
+                    pm += "cmd.Parameters.Add(" + ch.ToString() + "@p_" + dt.Rows[i]["COLUMN_NAME"].ToString() + "" + ch.ToString() + ", MySqlDbType.Int32).Value = obj." + dt.Rows[i]["COLUMN_NAME"].ToString() + "; \n";
+                else if (dt.Rows[i]["DATA_TYPE"].ToString() == "varchar")
+                    pm += "cmd.Parameters.Add(" + ch.ToString() + "@p_" + dt.Rows[i]["COLUMN_NAME"].ToString() + "" + ch.ToString() + ", MySqlDbType.VarChar, " + dt.Rows[i]["CHARACTER_MAXIMUM_LENGTH"].ToString() + ").Value = obj." + dt.Rows[i]["COLUMN_NAME"].ToString() + "; \n";
+                else if (dt.Rows[i]["DATA_TYPE"].ToString() == "timestamp" || dt.Rows[i]["DATA_TYPE"].ToString() == "datetime")
+                    pm += "cmd.Parameters.Add(" + ch.ToString() + "@p_" + dt.Rows[i]["COLUMN_NAME"].ToString() + "" + ch.ToString() + ", MySqlDbType.DateTime).Value = obj." + dt.Rows[i]["COLUMN_NAME"].ToString() + "; \n";
+                else if (dt.Rows[i]["DATA_TYPE"].ToString() == "tinyint")
+                    pm += "cmd.Parameters.Add(" + ch.ToString() + "@p_" + dt.Rows[i]["COLUMN_NAME"].ToString() + "" + ch.ToString() + ", MySqlDbType.Int16).Value = obj." + dt.Rows[i]["COLUMN_NAME"].ToString() + "; \n";
+                else
+                    pm += "//contact Vijith \n";
+            }
+            pm += "return excQry(cmd);";
+            txtParameter.Text = pm;
+        }
+
+        private void dataModel(System.Data.DataTable dt)
+        {
+            string dm = "";
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                dm += "private ";
+                if (dt.Rows[i]["DATA_TYPE"].ToString() == "int")
+                    dm += "int _" + dt.Rows[i]["COLUMN_NAME"].ToString()+";\n";
+                else if(dt.Rows[i]["DATA_TYPE"].ToString() == "varchar")
+                    dm += "string _" + dt.Rows[i]["COLUMN_NAME"].ToString() + ";\n";
+                else if (dt.Rows[i]["DATA_TYPE"].ToString() == "timestamp" || dt.Rows[i]["DATA_TYPE"].ToString() == "datetime")
+                    dm += "DateTime _" + dt.Rows[i]["COLUMN_NAME"].ToString() + ";\n";
+                else if (dt.Rows[i]["DATA_TYPE"].ToString() == "tinyint")
+                    dm += "bool _" + dt.Rows[i]["COLUMN_NAME"].ToString() + ";\n";
+                else
+                    dm += "// contact Vijith \n";
+            }
+            dm += "\n";
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                dm += "\n public ";
+                if (dt.Rows[i]["DATA_TYPE"].ToString() == "int")
+                    dm += "int " + dt.Rows[i]["COLUMN_NAME"].ToString() + "";
+                else if (dt.Rows[i]["DATA_TYPE"].ToString() == "varchar")
+                    dm += "string " + dt.Rows[i]["COLUMN_NAME"].ToString() + "";
+                else if (dt.Rows[i]["DATA_TYPE"].ToString() == "timestamp" || dt.Rows[i]["DATA_TYPE"].ToString() == "datetime")
+                    dm += "DateTime " + dt.Rows[i]["COLUMN_NAME"].ToString() + "";
+                else if (dt.Rows[i]["DATA_TYPE"].ToString() == "tinyint")
+                    dm += "bool " + dt.Rows[i]["COLUMN_NAME"].ToString() + "";
+                else
+                    dm += "// contact Vijith \n";
+                dm += @"
+                    {
+                    get { return _"+dt.Rows[i]["COLUMN_NAME"].ToString()+@"; }
+                    set { _"+dt.Rows[i]["COLUMN_NAME"].ToString()+@" = value; }
+                    } ";
+            }
+            txtModel.Text = dm;
+        }
+
+        private void storedPros(DataTable dt)
+        {
+            string sp = "CREATE DEFINER=`root`@`%` PROCEDURE `procedure Name`(";
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (dt.Rows[i]["DATA_TYPE"].ToString() == "varchar")
+                    sp += "IN p_" + dt.Rows[i]["COLUMN_NAME"].ToString() + " " + dt.Rows[i]["DATA_TYPE"].ToString() + "(" + dt.Rows[i]["CHARACTER_MAXIMUM_LENGTH"].ToString() + ")";
+                else
+                    sp += "IN p_" + dt.Rows[i]["COLUMN_NAME"].ToString() + " " + dt.Rows[i]["DATA_TYPE"].ToString() + "";
+                if (dt.Rows.Count != (i + 1))
+                    sp += ",\n";
+            }
+            sp += @")
+                BEGIN
+                if p_" + dt.Rows[0]["COLUMN_NAME"].ToString() + "='0' THEN ";
+            sp += "Insert into " + txtTable.Text + " (\n";
+            for (int i = 1; i < dt.Rows.Count; i++)
+            {
+                sp += dt.Rows[i]["COLUMN_NAME"].ToString();
+                if (dt.Rows.Count != (i + 1))
+                    sp += ",";
+            }
+            sp += ") values ( \n";
+            for (int i = 1; i < dt.Rows.Count; i++)
+            {
+                sp += "p_" + dt.Rows[i]["COLUMN_NAME"].ToString();
+                if (dt.Rows.Count != (i + 1))
+                    sp += ",";
+            }
+            sp += @");
+                else
+                update " + txtTable.Text.ToString() + " set ";
+            for (int i = 1; i < dt.Rows.Count; i++)
+            {
+                sp += dt.Rows[i]["COLUMN_NAME"].ToString() + "=" + "p_" + dt.Rows[i]["COLUMN_NAME"].ToString();
+                if (dt.Rows.Count != (i + 1))
+                    sp += ",";
+            }
+            sp += "\n where  " + dt.Rows[0]["COLUMN_NAME"].ToString() + "=" + "p_" + dt.Rows[0]["COLUMN_NAME"].ToString() + ";\n";
+            sp += @"end if;
+                    END";
+            txtSP.Text = sp;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTable.Text = "";
+            txtSP.Text = "";
+            txtModel.Text = "";
+            txtParameter.Text = "";
+        }
+
+        private void txtScema_Leave(object sender, EventArgs e)
+        {
+            //System.Threading.Thread t = new System.Threading.Thread(new System.Threading.ThreadStart(autoComplete));
+            //try
+            //{
+            //    t.Start();
+            //}
+            //catch (Exception ex)
+            //{                
+            //}
+        }
+
+        private void autoComplete()
+        {
+            string qry = @"SELECT   TABLE_NAME               
+               FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='" + txtScema.Text + "'";
+            DataTable dt = getTableQry(qry);
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                    txtTable.AutoCompleteCustomSource.Add(dt.Rows[i][0].ToString());
+            }
+        }
+    }
+}
